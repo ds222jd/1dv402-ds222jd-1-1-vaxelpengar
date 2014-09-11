072e94895941c9b60a4c17e4a063cd518fd2aef9
ds222jd <ds222jd@student.lnu.se>
2014-09-11 20:45:41 +0200

    Lite små ändringar

--- Program.cs
+++ Program.cs
@@ -15,72 +15,95 @@ namespace Växelpengar
             uint recieved;
             double roundingOffAmount;
             uint cash;
-           
+
             // Skaffar indata
 
-            
+
             {
-                
-                
+
+
                 Console.Write("Ange totalsumma     : ");
                 total = double.Parse(Console.ReadLine());
                 if (total < 0.51)
-                
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("Totalsumman är för liten");
                     Console.ResetColor();
-
-                } 
-                else 
-                
-
-
-
-                    Console.Write("Ange erhållet belopp: ");
-                    recieved = uint.Parse(Console.ReadLine());
-
-                    cash = recieved;
+                     
+                }
+                else
+                {
 
 
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.Write("Ange erhållet belopp: ");
+                            recieved = uint.Parse(Console.ReadLine());
+                            cash = recieved;
+
+                        break;
+
+                        }
+                        catch
+                        {
+                            //felmeddelande
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Erhållet belopp felaktigt");
+                            Console.ResetColor();
+                            
+
+
+                        }
 
 
 
+                    }
+
+                    {
+                        {
 
 
 
+                        }
+                        //Presenterar kvitto
 
-                    //Presenterar kvitto
+                        Console.WriteLine("KVITTO");
 
-                    Console.WriteLine("KVITTO");
+                        Console.WriteLine("-------------------------------------------------");
 
-                    Console.WriteLine("-------------------------------------------------");
-
-                    Console.WriteLine("Totalt        : {0:c} ", total);
-                    recieved = (uint)Math.Round(total);
-                    roundingOffAmount = recieved - total;
-
-
-
-                    Console.WriteLine("Öresavrundning: {0:c} ", roundingOffAmount);
-
-                    Console.WriteLine("Att betala    : {0:c} ", recieved - (uint)roundingOffAmount);
+                        Console.WriteLine("Totalt        : {0:c} ", total);
+                        recieved = (uint)Math.Round(total);
+                        roundingOffAmount = recieved - total;
 
 
-                    Console.WriteLine("Kontant       : {0:c}", cash);
+
+                        Console.WriteLine("Öresavrundning: {0:c} ", roundingOffAmount);
+
+                        Console.WriteLine("Att betala    : {0:c} ", recieved - (uint)roundingOffAmount);
 
 
-                    Console.WriteLine("Tillbaka      : {0:c}", cash - (uint)Math.Round(total));
+                        Console.WriteLine("Kontant       : {0:c}", cash);
 
-                    Console.WriteLine("-------------------------------------------------");
+
+                        Console.WriteLine("Tillbaka      : {0:c}", cash - (uint)Math.Round(total));
+
+                        Console.WriteLine("-------------------------------------------------");
+
+                    }
+
+
 
                 }
-
-
-
             }
         }
     }
+}
+        
+    
+
 
     
 
